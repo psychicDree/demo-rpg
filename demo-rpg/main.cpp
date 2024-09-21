69b46cb22882a33d2d2dd10bc960299df69bf31b
manish <devmanjha@gmail.com>
2024-09-21 14:15:00 +0530

    Initial Commit

---
+++ demo-rpg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  demo-rpg
+//
+//  Created by Manish Jha on 21/09/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
